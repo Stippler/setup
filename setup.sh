0a5158a24dff1581f9182e943b0d52197fdc1201
Christian Stippel <e11778254@tuwien.ac.at>
2019-07-14 22:14:49 +0200

    added setup script

---
+++ setup.sh
@@ -0,0 +1,26 @@
+apt update
+
+# fish setup:
+apt-get install fish 
+chsh -s /usr/bin/fish 
+
+# i3 setup:
+apt install i3
+
+# install java jdk 11
+add-apt-repository ppa:openjdk-r/ppa
+apt-get update -q
+apt install -y openjdk-11-jdk
+
+# install python3
+apt-get install python3
+
+# install python2
+apt-get install python2
+
+apt-get install arandr
+# reboot
+# shutdown now -r
+
+snap install pycharm-community --classic
+snap install intellij-idea-ultimate --classic
